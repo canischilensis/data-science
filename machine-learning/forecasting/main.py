7e6c442017cc92b084165d33ad1da4b4eb4d86a9
Canis chilensis <45744107+canischilensis@users.noreply.github.com>
2026-03-03 23:14:47 -0300

    hola

--- machine-learning/forecasting/main.py
+++ machine-learning/forecasting/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import xgboost as xgb
-from sklearn.metrics import mean_squared_error, mean_absolute_error # <-- NEW IMPORT
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 # --- 1. MENTAL CONFIGURATION ---
 KEYS = ["store_id", "sku"]
